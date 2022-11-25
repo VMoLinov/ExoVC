e220d5329f39e2859266cbc5408dd9630d61e8bb
vadim <molinov@list.ru>
2022-11-25 13:04:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/test/exovc/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.test.exovc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.test.exovc.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/test/exovc/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.test.exovc.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
